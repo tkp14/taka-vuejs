32ac168cdfe4a2f1ba9a7d1f191454c9d2c9f178
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2021-03-09 17:58:12 +0900

    コンポーネントについての学習4

--- src/App.vue
+++ src/App.vue
@@ -1,126 +1,101 @@
 <template>
-  <div>
-    <LikeHeader>
-      <h2>こんにちは</h2>
-    </LikeHeader>
-    <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
-    <button @click="currentComponent = 'Home'">Home</button>
-    <button @click="currentComponent = 'About'">About</button>
-    <keep-alive>
-      <component :is="currentComponent"></component>
-    </keep-alive>
-    <div style="padding: 10rem;">
-      <h2>イベントのフォーム</h2>
-      <EventTitle v-model="eventData.title"></EventTitle>
-      <label for="maxNumber">最大人数</label>
-      <input
-        id="maxNumber"
-        type="number"
-        v-model.number="eventData.maxNumber"
-      >
-      <p>{{ eventData.maxNumber }}</p>
-      <label for="host">主催者</label>
-      <input
-        id="host"
-        type="text"
-        v-model.trim="eventData.host"
-      >
-      <pre>{{ eventData.host }}</pre>
-      <label for="detail">イベントの内容</label>
-      <textarea id="detail" cols="30" rows="10" v-model="eventData.detail"></textarea>
-      <pre>{{ eventData.detail }}</pre>
-      <input
-        type="checkbox"
-        id="isPrivate"
-        v-model="eventData.isPrivate"
-      >
-      <label for="isPrivate">非公開</label>
-      <p>{{ eventData.isPrivate }}</p>
-      <p>参加条件</p>
-      <input
-        type="checkbox"
-        id="10"
-        value="10代"
-        v-model="eventData.target"
-      >
-      <label for="10">10代</label>
-      <input
-        type="checkbox"
-        id="20"
-        value="20代"
-        v-model="eventData.target"
-      >
-      <label for="20">20代</label>
-      <input
-        type="checkbox"
-        id="30"
-        value="30代"
-        v-model="eventData.target"
-      >
-      <label for="30">30代</label>
-      <p>{{ eventData.target }}</p>
-      <p>参加費</p>
-      <input
-        type="radio"
-        id="free"
-        value="無料"
-        v-model="eventData.price"
-      >
-      <label for="free">無料</label>
-      <input
-        type="radio"
-        id="paid"
-        value="有料"
-        v-model="eventData.price"
-      >
-      <label for="paid">有料</label>
-      <p>開催場所</p>
-      <select
-       v-model="eventData.location"
-       multiple
-      >
-        <option v-for="location in locations" :key="location"
-        >{{ location }}</option>
-      </select>
-      <p>{{ eventData.location }}</p>
-    </div>
+  <div class="main">
+    <button @click="myAnimation ='slide'">Slide</button>
+    <button @click="myAnimation ='fade'">Fade</button>
+    <p>{{ myAnimation }}</p>
+    <button @click="show = !show">切り替え</button>
+    <br>
+    <button @click="myComponent ='ComponentA'">ComponentA</button>
+    <button @click="myComponent ='ComponentB'">ComponentB</button>
+    <transition name="fade" mode="out-in">
+      <component :is="myComponent"></component>
+    </transition>
+    <transition name="fade" mode="out-in">
+      <p v-if="show" key="bye">さよなら</p>
+      <p v-if="!show" key="hello">こんにちは</p>
+    </transition>
+    <transition
+      enter-active-class="animate__animated animate__bounce"
+      leave-active-class="animate__animated animate__flash"
+      appear
+    >
+      <p v-if="show">hello</p>
+    </transition>
+    <transition
+      :name="myAnimation"
+      appear
+    >
+      <p v-show="show">bye</p>
+    </transition>
   </div>
 </template>
 
 <script>
-import LikeHeader from "./components/LikeHeader.vue";
-import Home from "./components/Home.vue";
-import About from "./components/About.vue";
-import EventTitle from "./components/EventTitle.vue";
+import ComponentA from "./components/ComponentA.vue";
+import ComponentB from "./components/ComponentB.vue";
 
 export default {
+  components: {
+    ComponentA,
+    ComponentB
+  },
   data() {
     return {
-      number: 10,
-      currentComponent: "Home",
-      locations: ["東京", "大阪", "名古屋"],
-      eventData: {
-        title: "",
-        maxNumber: 0,
-        host: "",
-        detail: "",
-        isPrivate: false,
-        target: [],
-        price: "無料",
-        location: []
-      }
+      show: true,
+      myAnimation: "slide",
+      myComponent: "ComponentA"
     };
-  },
-  components: {
-    LikeHeader,
-    Home,
-    About,
-    EventTitle
-  },
-  methods: {
-    incrementNumber(value) {
-      this.number = value;
-    }
   }
 };
 </script>
+
+<style scoped>
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 0.5s;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-leave {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-leave-to {
+ opacity: 0;
+}
+
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+.slide-enter-active {
+  animation: slide-in 0.5s;
+  transition: opacity 1s;
+}
+.slide-leave-active {
+  animation: slide-in 0.5s reverse;
+  transition: opacity 1s;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateX(100px);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+.main {
+  width: 70%;
+  margin: auto;
+  padding-top: 5rem;
+  text-align: center;
+}
+
+</style>
