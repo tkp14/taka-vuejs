01fb07e0e89dee32d9874a7a59de4ac0db1b39d0
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2021-03-09 23:46:53 +0900

    Vue.jsの学習5

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,29 @@
     <button @click="myAnimation ='slide'">Slide</button>
     <button @click="myAnimation ='fade'">Fade</button>
     <p>{{ myAnimation }}</p>
+    <br>
+    <button @click="add">追加</button>
+    <ul style="width: 200px; margin: auto;">
+      <transition-group name="fade">
+        <li
+          style="cursor: pointer;"
+          v-for="(number, index) in numbers"
+          @click="remove(index)"
+          :key="number"
+        >{{ number }}</li>
+      </transition-group>
+    </ul>
+    <br>
     <button @click="show = !show">切り替え</button>
+    <br><br>
+    <transition
+      :css="false"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @leave="leave"
+    >
+      <div class="circle" v-if="show"></div>
+    </transition>
     <br>
     <button @click="myComponent ='ComponentA'">ComponentA</button>
     <button @click="myComponent ='ComponentB'">ComponentB</button>
@@ -41,20 +63,70 @@ export default {
   },
   data() {
     return {
+      numbers: [0, 1, 2],
+      nextNumber: 3,
       show: true,
       myAnimation: "slide",
       myComponent: "ComponentA"
     };
+  },
+  methods: {
+    randomIndex() {
+      return Math.floor(Math.random() * this.numbers.length)
+    },
+    add() {
+      this.numbers.splice(this.randomIndex(), 0, this.nextNumber);
+      this.nextNumber += 1;
+    },
+    remove(index) {
+      this.numbers.splice(index, 1);
+    },
+    beforeEnter(el) {
+      el.style.transform = 'scale(0)'
+    },
+    enter(el, done) {
+      let scale = 0;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale += 0.1
+        if ( scale > 1) {
+          clearInterval(interval);
+          done();
+        }
+      }, 30);
+    },
+    leave(el, done) {
+      let scale = 1;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale -= 0.1
+        if ( scale < 0) {
+          clearInterval(interval);
+          done();
+        }
+      }, 30);
+    }
   }
 };
 </script>
 
 <style scoped>
+.circle {
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  border-radius: 100px;
+  background-color: deeppink;
+}
+
+.fade-move {
+  transition: transform 1s;
+}
 .fade-enter {
   opacity: 0;
 }
 .fade-enter-active {
-  transition: opacity 0.5s;
+  transition: opacity 1s;
 }
 .fade-enter-to {
   opacity: 1;
@@ -63,7 +135,9 @@ export default {
   opacity: 1;
 }
 .fade-leave-active {
-  transition: opacity 0.5s;
+  transition: opacity 1s;
+  position: absolute;
+  width: 200px;
 }
 .fade-leave-to {
  opacity: 0;
